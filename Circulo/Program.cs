06f2c4dcc743b7554ca94f51475c2c5f5a13299f
Tony <antony.branquinho@gmail.com>
2023-11-28 14:14:25 -0300

    bem facil

---
+++ Circulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circulo {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double raio, area;
+
+            Console.Write("Digite o valor do raio do circulo: ");
+            raio = double.Parse(Console.ReadLine(), CI);
+
+            area = (3.14159 * (raio * raio));
+
+            Console.WriteLine("AREA = " + area.ToString("F3", CI));
+        }
+    }
+}
